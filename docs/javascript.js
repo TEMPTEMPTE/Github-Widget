6c4c5a6ff6cce76832b7e5d79b124b116368f61f
Zseni <83803826+Zseni051@users.noreply.github.com>
2022-01-18 16:26:34 +1000

    Create javascript.js

---
+++ docs/javascript.js
@@ -0,0 +1,29 @@
+var doc = document;
+var styleSheet = doc.createElement("style");
+var docURL = window.location.href;
+var params = [];
+
+docURL = docURL.replace(window.location.origin, '');
+
+if (docURL.indexOf('/#/') > -1) {
+  docURL = docURL.split('/#/')[1];
+  if (docURL != '') {
+    if (docURL[docURL.length - 1] == '/') {
+      docURL = docURL.substring(0, docURL.length - 1);
+    }
+    params = docURL.split('/');
+
+    if(params[1] != null){
+      styleSheet.type = "text/css"
+      styleSheet.innerText = params[1]
+      document.head.appendChild(styleSheet)
+    }
+
+    if(params[0] != null){
+      doc.getElementById("add2me").innerHTML = 
+      `<div class="github-widget" data-username="${params[0]}"></div>`
+    }
+  }
+} else {
+    doc.getElementById("add2me").innerHTML = `<p>No username given</p>`;
+}

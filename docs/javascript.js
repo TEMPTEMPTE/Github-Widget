cc59e7acae616a9533dd66d53eae7231d91fec72
Zseni <83803826+Zseni051@users.noreply.github.com>
2022-01-18 16:50:34 +1000

    Update javascript.js

--- docs/javascript.js
+++ docs/javascript.js
@@ -3,15 +3,22 @@ var styleSheet = doc.createElement("style");
 var docURL = window.location.href;
 var params = [];
 
+// filter out the website origin "example.github.io"
 docURL = docURL.replace(window.location.origin, '');
 
+// if /#/ found then we have URL parameters
+// grabbing the parameters part of the URL
 if (docURL.indexOf('/#/') > -1) {
   docURL = docURL.split('/#/')[1];
   if (docURL != '') {
+
+    // omit the last forward slash if exist
     if (docURL[docURL.length - 1] == '/') {
       docURL = docURL.substring(0, docURL.length - 1);
     }
+    // split the URL final string o get an object with all params 
     params = docURL.split('/');
+    console.log(params);
 
     if(params[1] != null){
       styleSheet.type = "text/css"
@@ -25,5 +32,6 @@ if (docURL.indexOf('/#/') > -1) {
     }
   }
 } else {
-    doc.getElementById("add2me").innerHTML = `<p>No username given</p>`;
+  console.log('No URL parameters found');
+    doc.getElementById("add2me").innerHTML = `<p>No URL parameters found</p>`;
 }

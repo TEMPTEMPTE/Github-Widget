3c501fca55e184bab50f67bdbef46f40638d49a7
Zseni <83803826+Zseni051@users.noreply.github.com>
2022-02-08 17:07:58 +1000

    Update userparams.js

--- docs/userparams.js
+++ docs/userparams.js
@@ -8,43 +8,63 @@ var params = [];
 docURL = docURL.replace(window.location.origin, '');
 
 // if /#/ found then we have URL parameters
-// grabbing the parameters part of the URL
-if (docURL.indexOf('/#/') !== -1) { 
-  docURL = docURL.split('/#/')[1];
+if (docURL.indexOf('/api/?') !== -1) { 
+  docURL = docURL.split('api/?')[1];
   if (docURL != '') {
 
-    // if "link: exist remove it from docUrl and add it to css_link"
-    if(docURL.indexOf('link:') !== -1) {
-      css_link = docURL.split('/link:')[1];
-      docURL = docURL.split('/link:')[0];
+    // if "&css="" exist remove it from docUrl and add it to css_link
+    if(docURL.indexOf('css=') !== -1) {
+      css_link = `css=${docURL.split('&css=')[1]}`;
+      docURL = docURL.split('&css=')[0];
     }
-    
-    // split the URL final string o get an object with all params 
-    params = docURL.split('/');
+
+    // split the URL final string to get an object with all params 
+    params = docURL.split('&');
 
     // add css_link to params
-    params.push(css_link)
-    console.log("params:",params);
-
-    if(params[3] != ""){
-      doc.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", `<link rel="stylesheet" href=${params[2]} />`);
+    if(css_link != '') {
+      params.push(css_link);
     }
 
-    if(params[2] != ""){
+    // loop through the params, give them a variable and console logs them
+    for (var i = 0; i < params.length; i++) {
+      var param = params[i].split('=');
+      var param_name = param[0];
+      var param_value = param[1];
+      if (param_name == 'username') {
+        var username = param_value;
+        console.log(param_name + ': ' + param_value);
+      }
+      if (param_name == 'toprepos') {
+        var toprepos = param_value;
+        console.log(param_name + ': ' + param_value);
+      }
+      if (param_name == 'style') {
+        var style = param_value;
+        console.log(param_name + ': ' + param_value);
+      }
+      if (param_name == 'css') {
+        var css = param_value;
+        console.log(param_name + ': ' + param_value);
+      }
+    }
+
+    if(css){
+      doc.getElementsByTagName("head")[0].insertAdjacentHTML("beforeend", `<link rel="stylesheet" href=${css}/>`);
+    }
+    if(style){
       styleSheet.type = "text/css"
-      styleSheet.innerText = params[1]
+      styleSheet.innerText = style
       doc.head.appendChild(styleSheet)
     }
-
-    if(params[0]){
-      if(params[1] == ""){
-        params[1] = 3
+    if(username){
+      if(!toprepos){
+        toprepos = 3
       }
-      doc.getElementById("add2me").innerHTML = 
-      `<div class="github-widget" data-username="${params[0]}" data-toprepos="${params[1]}"></div>`
+      doc.getElementById("add2me").innerHTML = `<div class="github-widget" data-username="${username}" data-toprepos="${toprepos}"></div>`
     }
   }
 } else {
   console.log('No URL parameters found');
-    doc.getElementById("add2me").innerHTML = `<p>No URL parameters found</p>`;
+    doc.getElementById("add2me").innerHTML = `<a>No URL parameters found</a>`;
 }
